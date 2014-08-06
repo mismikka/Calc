3dd8d6b8fad29e51f79ff61b05a777eef6c012df
Aurococcus <drr1@yandex.ru>
2014-08-06 22:31:35 +0800

    Add & Sub fix, ReplaceLastChar()

--- MainActivity.cs
+++ MainActivity.cs
@@ -8,21 +8,34 @@ using Android.Widget;
 using Android.OS;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Calc
 {
     [Activity(Label = "Calc", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    internal sealed class MainActivity : Activity
     {
-        int count = 1;
+        private enum Keys
+        { 
+            Digit, 
+            Operator, 
+            Equal, 
+            DecimalPoint,
+            Sign
+        }
 
+        private Keys? _lastKeyInput = null;
+        IOperation op = null;
+        List<string> parameters = new List<string>();
+        private double _total = 0;
+        
         protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
-
+            
 			Button button0 = FindViewById<Button>(Resource.Id.Button0);
 			Button button1 = FindViewById<Button>(Resource.Id.Button1);
 			Button button2 = FindViewById<Button>(Resource.Id.Button2);
@@ -45,117 +58,185 @@ namespace Calc
 
 			Button buttonC = FindViewById<Button>(Resource.Id.ButtonC);
 
-			TextView resultField = FindViewById<TextView>(Resource.Id.ResultField);
+            TextView resultField = FindViewById<TextView>(Resource.Id.textView1);
 			TextView txt = FindViewById<TextView>(Resource.Id.TextView);
-			resultField.Enabled = false;
-
-			List<string> parameters = new List<string>();
 
 			#region button[0-9] handlers
-			button0.Click += (object sender, EventArgs e) => {
-				resultField.Text += 0;
-			};
+            
+			button0.Click += (object sender, EventArgs e) =>
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+                    resultField.Text += 0;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button1.Click += (object sender, EventArgs e) => {
-				resultField.Text += 1;
-			};
+			button1.Click += (object sender, EventArgs e) =>
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+                    resultField.Text += 1;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button2.Click += (object sender, EventArgs e) => {
-				resultField.Text += 2;
-			};
+			button2.Click += (object sender, EventArgs e) =>
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+                    resultField.Text += 2;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button3.Click += (object sender, EventArgs e) => {
-				resultField.Text += 3;
-			};
+			button3.Click += (object sender, EventArgs e) => 
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+				    resultField.Text += 3;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button4.Click += (object sender, EventArgs e) => {
-				resultField.Text += 4;
-			};
+			button4.Click += (object sender, EventArgs e) => 
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+				    resultField.Text += 4;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button5.Click += (object sender, EventArgs e) => {
-				resultField.Text += 5;
-			};
+			button5.Click += (object sender, EventArgs e) =>
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+				    resultField.Text += 5;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button6.Click += (object sender, EventArgs e) => {
-				resultField.Text += 6;
-			};
+			button6.Click += (object sender, EventArgs e) => 
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+				    resultField.Text += 6;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button7.Click += (object sender, EventArgs e) => {
-				resultField.Text += 7;    
-			};
+			button7.Click += (object sender, EventArgs e) => 
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+				    resultField.Text += 7;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button8.Click += (object sender, EventArgs e) => {
-				resultField.Text += 8;
-			};
+			button8.Click += (object sender, EventArgs e) => 
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+				    resultField.Text += 8;
+                    _lastKeyInput = Keys.Digit;
+                };
 
-			button9.Click += (object sender, EventArgs e) => {
-				resultField.Text += 9;
-			};
+			button9.Click += (object sender, EventArgs e) => 
+                {
+                    if (_lastKeyInput == Keys.Operator)
+                        resultField.Text = "";
+                    resultField.Text += 9;
+                    _lastKeyInput = Keys.Digit;
+                };
 
 			#endregion
+            
+            buttonAddition.Click += (object sender, EventArgs e) =>
+            {
+                if (_lastKeyInput == Keys.Digit)
+                {
+                    parameters.Add(resultField.Text);
+                    txt.Text += resultField.Text + "+";
+                    resultField.Text = "";
+                        
+                    if (op == null || op.GetType() != typeof(Addition))
+                        op = new Addition();
+                        
+                    if (parameters.Count == 2)
+                    {
+                        double[] p = this.Parse(parameters);
+                        double r = op.Calculate(p);
+                        resultField.Text = r.ToString();
+                        parameters.Clear();
+                        parameters.Add(r.ToString());
+                    }                        
+                    _lastKeyInput = Keys.Operator;
+                    return;
+                }
+                    
+                if (_lastKeyInput == Keys.Operator)
+                {
+                    if (op == null || op.GetType() != typeof(Addition))
+                    {
+                        op = new Addition();
+                        txt.Text = ReplaceLastChar(txt.Text, '+');              
+                    }
+                }           
+            };
+            
+            buttonSubstraction.Click += (object sender, EventArgs e) =>
+            {
+                if (_lastKeyInput == Keys.Digit)
+                {
+                    parameters.Add(resultField.Text);
+                    txt.Text += resultField.Text + "-";
+                    resultField.Text = "";
 
-			IOperation op = null;
+                    if (op == null || op.GetType() != typeof(Subtraction))
+                        op = new Subtraction();
 
-			buttonAddition.Click += (object sender, EventArgs e) =>
-			{
-				if (parameters.Count == 0 && string.IsNullOrEmpty(resultField.Text))
-					return;
-				if (op == null)
-				{
-					op = new Addition();
-					parameters.Add(resultField.Text);
-					txt.Text += resultField.Text + "+";
-					resultField.Text = string.Empty;
-				}
-				else if (!(op.GetType() == typeof(Addition)))
-				{
-					op = new Addition();
-					StringBuilder sb = new StringBuilder();
-					sb.Append(txt.Text);
-					sb[sb.Length - 1] = '+';
-					txt.Text = sb.ToString();
-				}
-			};
+                    if (parameters.Count == 2)
+                    {
+                        double[] p = this.Parse(parameters);
+                        double r = op.Calculate(p);
+                        resultField.Text = r.ToString();
+                        parameters.Clear();
+                        parameters.Add(r.ToString());
+                        _total = op.Calculate(_total, r);
+                        resultField.Text = _total.ToString();
+                    }
+                }
 
-			buttonSubstraction.Click += (object sender, EventArgs e) =>
-			{
-				if (parameters.Count == 0 && string.IsNullOrEmpty(resultField.Text))
-					return;
-				if (op == null)
-				{
-					op = new Subtraction();
-					parameters.Add(resultField.Text);
-					txt.Text += resultField.Text + "-";
-					resultField.Text = string.Empty;
-				}
-				else if (!(op.GetType() == typeof(Subtraction)))
-				{
-					op = new Subtraction();
-					StringBuilder sb = new StringBuilder();
-					sb.Append(txt.Text);
-					sb[sb.Length - 1] = '-';
-					txt.Text = sb.ToString();
-				}
-			};
-
-			buttonEqual.Click += (object sender, EventArgs e) =>
-			{
-
-				parameters.Add(resultField.Text);
-				txt.Text = string.Empty;
-				//txt.Text = Eval(op, this.Parse(parameters)).ToString();
-				resultField.Text = Eval(op, this.Parse(parameters)).ToString();
-				parameters.Clear();
-
-			};
-
-			buttonC.Click += (object sender, EventArgs e) =>
-			{
-				op = null;
-				resultField.Text = string.Empty;
-				txt.Text = string.Empty;
-				parameters.Clear();
-			};
+                if (_lastKeyInput == Keys.Operator)
+                {
+                    if (op == null || op.GetType() != typeof(Subtraction))
+                    {
+                        op = new Subtraction();
+                        txt.Text = ReplaceLastChar(txt.Text, '-');              
+                    }
+                }
+            };
+            
+            
+            buttonEqual.Click += (object sender, EventArgs e) =>
+            {
+                if (_lastKeyInput == Keys.Digit)
+                {
+                    parameters.Add(resultField.Text);
+                    txt.Text = string.Empty;
+                    double r = Eval(op, this.Parse(parameters));
+                    resultField.Text = r.ToString();
+                }
+                if (_lastKeyInput == Keys.Operator)
+                {
+                    txt.Text = "";
+                    resultField.Text = op.Calculate(double.Parse(parameters[0]), double.Parse(resultField.Text)).ToString();
+                    parameters.Clear();
+                }
+            };
+            
+            // Clear
+            buttonC.Click += (object sender, EventArgs e) =>
+            {
+                op = null;
+                resultField.Text = string.Empty;
+                txt.Text = string.Empty;
+                parameters.Clear();
+            };
 		}
 
 		public double Eval(IOperation op, params double[] parameters)
@@ -164,13 +245,20 @@ namespace Calc
 		}
 
 		public double[] Parse(List<string> parameters)
-		{
-			List<double> result = new List<double>();
-			foreach (var p in parameters)
-			{
-				result.Add(double.Parse(p));
-			}
-			return result.ToArray();
-		}
-	}
+        {
+            List<double> result = new List<double>();
+            foreach (var p in parameters)
+            {
+                result.Add(double.Parse(p));
+            }
+            return result.ToArray();
+        }
+        
+        public string ReplaceLastChar(string str, char ch)
+        {
+            StringBuilder sb = new StringBuilder(str);
+            sb[sb.Length - 1] = ch;
+            return sb.ToString();
+        }
+    }
 }
--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -60,83 +60,86 @@ namespace Calc
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050016
-			public const int Button0 = 2131034134;
-			
 			// aapt resource value: 0x7f050017
-			public const int Button1 = 2131034135;
-			
-			// aapt resource value: 0x7f050012
-			public const int Button2 = 2131034130;
-			
-			// aapt resource value: 0x7f050013
-			public const int Button3 = 2131034131;
-			
-			// aapt resource value: 0x7f05000e
-			public const int Button4 = 2131034126;
-			
-			// aapt resource value: 0x7f05000f
-			public const int Button5 = 2131034127;
-			
-			// aapt resource value: 0x7f05000a
-			public const int Button6 = 2131034122;
-			
-			// aapt resource value: 0x7f05000b
-			public const int Button7 = 2131034123;
-			
-			// aapt resource value: 0x7f050006
-			public const int Button8 = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int Button9 = 2131034119;
-			
-			// aapt resource value: 0x7f050008
-			public const int ButtonAdd = 2131034120;
-			
-			// aapt resource value: 0x7f050015
-			public const int ButtonBackspace = 2131034133;
-			
-			// aapt resource value: 0x7f050005
-			public const int ButtonC = 2131034117;
+			public const int Button0 = 2131034135;
 			
 			// aapt resource value: 0x7f050018
-			public const int ButtonComma = 2131034136;
+			public const int Button1 = 2131034136;
 			
-			// aapt resource value: 0x7f05000d
-			public const int ButtonDiv = 2131034125;
-			
-			// aapt resource value: 0x7f050019
-			public const int ButtonEqual = 2131034137;
-			
-			// aapt resource value: 0x7f050011
-			public const int ButtonFraction = 2131034129;
-			
-			// aapt resource value: 0x7f050002
-			public const int ButtonMC = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int ButtonMR = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int ButtonMS = 2131034116;
-			
-			// aapt resource value: 0x7f05000c
-			public const int ButtonMulti = 2131034124;
+			// aapt resource value: 0x7f050013
+			public const int Button2 = 2131034131;
 			
 			// aapt resource value: 0x7f050014
-			public const int ButtonNegate = 2131034132;
+			public const int Button3 = 2131034132;
+			
+			// aapt resource value: 0x7f05000f
+			public const int Button4 = 2131034127;
 			
 			// aapt resource value: 0x7f050010
-			public const int ButtonSqrt = 2131034128;
+			public const int Button5 = 2131034128;
+			
+			// aapt resource value: 0x7f05000b
+			public const int Button6 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int Button7 = 2131034124;
+			
+			// aapt resource value: 0x7f050007
+			public const int Button8 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int Button9 = 2131034120;
 			
 			// aapt resource value: 0x7f050009
-			public const int ButtonSub = 2131034121;
+			public const int ButtonAdd = 2131034121;
 			
-			// aapt resource value: 0x7f050001
-			public const int ResultField = 2131034113;
+			// aapt resource value: 0x7f050016
+			public const int ButtonBackspace = 2131034134;
+			
+			// aapt resource value: 0x7f050006
+			public const int ButtonC = 2131034118;
+			
+			// aapt resource value: 0x7f050019
+			public const int ButtonComma = 2131034137;
+			
+			// aapt resource value: 0x7f05000e
+			public const int ButtonDiv = 2131034126;
+			
+			// aapt resource value: 0x7f05001a
+			public const int ButtonEqual = 2131034138;
+			
+			// aapt resource value: 0x7f050012
+			public const int ButtonFraction = 2131034130;
+			
+			// aapt resource value: 0x7f050003
+			public const int ButtonMC = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int ButtonMR = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int ButtonMS = 2131034117;
+			
+			// aapt resource value: 0x7f05000d
+			public const int ButtonMulti = 2131034125;
+			
+			// aapt resource value: 0x7f050015
+			public const int ButtonNegate = 2131034133;
+			
+			// aapt resource value: 0x7f050011
+			public const int ButtonSqrt = 2131034129;
+			
+			// aapt resource value: 0x7f05000a
+			public const int ButtonSub = 2131034122;
 			
 			// aapt resource value: 0x7f050000
-			public const int TextView = 2131034112;
+			public const int Layout = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int TextView = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView1 = 2131034114;
 			
 			static Id()
 			{
